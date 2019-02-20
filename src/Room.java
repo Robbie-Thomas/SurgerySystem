100606fb1c0f364651ba6773f60c26a679e53969
Robbie <robbiejt55@gmail.com>
2019-02-20 15:01:33 +0000

    Created the Room.Java file

---
+++ src/Room.java
@@ -0,0 +1,58 @@
+import org.jetbrains.annotations.Contract;
+
+import java.util.Objects;
+
+public class Room {
+
+    private int id;
+    private String roomName;
+    private String roomNumber;
+
+    public Room() {
+    }
+
+    public Room(String roomName, String roomNumber) {
+        this.roomName = roomName;
+        this.roomNumber = roomNumber;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    @Contract(value = "null -> false", pure = true)
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Room)) return false;
+        Room room = (Room) o;
+        return id == room.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
